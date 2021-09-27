a174c4b87f85a8e591224fc07d78e652bcf4c5a2
miriam <m.moreno.clavijo@gmail.com>
2021-09-27 18:39:34 +0200

    01-tipos-basicos

---
+++ de_0_a_experto/ts-intro/curso-angular-intro-typescript-ecmascript-1.0.1/src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,12 @@
+
+let nombre: string = "strider";
+
+const apellidos: string = "mi_constante";
+
+let hp: number | string =95;
+
+let estaVivo: boolean = true;
+
+hp ="otro string";
+
+console.log(nombre, hp);
