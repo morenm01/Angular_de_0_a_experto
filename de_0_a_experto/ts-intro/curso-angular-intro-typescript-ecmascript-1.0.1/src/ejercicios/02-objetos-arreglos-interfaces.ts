22ae93955efa9e4938189799925e79e15cf4c210
miriam <m.moreno.clavijo@gmail.com>
2021-09-29 16:27:05 +0200

    objetos arreglos e interfaces

---
+++ de_0_a_experto/ts-intro/curso-angular-intro-typescript-ecmascript-1.0.1/src/ejercicios/02-objetos-arreglos-interfaces.ts
@@ -0,0 +1,23 @@
+//let habilidades : (boolean | string |number)[]= ["bash", "counter", "healing"];
+//habilidades.push("1");
+
+let habilidades : ( string)[]= ["bash", "counter", "healing"];
+
+interface Personaje{
+ nombre: string;
+ hp: number;
+ habilidades : string[];
+ puebloNatal?: string;
+
+}
+
+const personaje: Personaje = {
+    nombre: 'Straider',
+    hp : 100,
+    habilidades : ["bash", "counter", "healing"]
+
+}
+
+personaje.puebloNatal = "La Palma del Condado";
+
+console.table(personaje);
